f7ee400a5eea10e7169bc9f0a2227c1d69a2e655
Duoshot <obiwannabe1337@hotmail,.com>
2015-11-06 12:03:29 -0700

    v1
    Added an object that contains references to other objects

---
+++ SimpleObjects.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Gorman on 2015-11-06.
+ */
+public class SimpleObjects {
+
+    Primitives primObjA;
+    Primitives primObjB;
+
+    public SimpleObjects()
+    {
+        //default constructor
+    }
+
+    public SimpleObjects(Primitives a, Primitives b)
+    {
+        this.primObjA = a;
+        this.primObjB = b;
+    }
+
+}
